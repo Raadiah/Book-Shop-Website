3b38a5736e0d1b9e6fc5cf82f703b248c4c3a83d
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-09-22 20:36:31 +0600

    Responsive banner

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -6,10 +6,9 @@ function Banner() {
         <div className="hero-content flex-col lg:flex-row-reverse">
             <img
             src={bookImage}
-            className="max-w-xs max-h-xs rounded-lg shadow-2xl" />
+            className="max-w-xs max-h-xs mt-6 lg:mt-0 rounded-lg shadow-2xl" />
             <div>
-            <h1 className="text-5xl font-bold">Welcome!</h1>
-            <p className="py-6">
+            <p className="py-6 pr-6">
             Unlock a World of Stories – Find Your Next Favorite Book at Reader's Cafe
             </p>
             <button className="btn btn-primary">Buy Book</button>
