c34faaa08f73290c676b25ae14b0f33de29b1320
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-09-22 13:57:58 +0600

    Daisy UI made to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
+import daisyui from './node_modules/daisyui'
+
 export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx}",
-    'node_modules/daisyui/dist/**/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 }
