1025c273a08cf9e9d33502e22d470e8989244087
Soomanib Kamruzzaman <soomanib.doict@gmail.com>
2024-09-23 16:59:52 +0600

    Added some routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import HomePage from './pages/HomePage.jsx';
+import Books from './components/books/books.jsx';
 import './index.css'
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import HomePage from './pages/HomePage.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage></HomePage>,
-    errorElement: <div>Not found</div>
+    errorElement: <div>Not found</div>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>
+      },
+      {
+        path: "booklist",
+        element: <Books></Books>
+      },
+      {
+        path: "bookdetails",
+        element: <HomePage></HomePage>
+      }
+    ]
   },
 ]);
 
